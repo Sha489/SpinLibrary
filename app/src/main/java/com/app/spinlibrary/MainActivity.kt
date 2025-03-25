7333fab26b272d6a6f6166b5a20283802d7d06f8
sharon rose <sharon.rose@STS-1573.sts.co.in>
2025-03-25 11:22:13 +0530

    Cherry picked old commit

--- app/src/main/java/com/app/spinlibrary/MainActivity.kt
+++ app/src/main/java/com/app/spinlibrary/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         luckyWheelView = findViewById(R.id.luckyWheel)
         setData()
     }
